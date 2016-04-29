76fbafdb65a9a48b761e2d35b988b3850febebc8
Vlada47 <laznickav@hotmail.cz>
2016-04-29 20:59:06 +0200

    Texts for help menu moved to special class. Created few of them... it's boring as all hell.

---
+++ src/view/HelpTexts.java
@@ -0,0 +1,44 @@
+package view;
+
+public class HelpTexts {
+	
+	public static final String ROOT_CONTENT = "<h2>Choose the topic from the menu...</h2>";
+	
+	public static final String ABOUT_CONTENT = "<p>This application was made as a semestral project for KIV/UUR subject</p>"
+												+ "<p><b>Author: Vladimír Láznièka</b></p>";
+	
+	public static final String GUIDE_CONTENT = "<p>This sub-menu contains a guide to both character building rules and application manual.</p>";
+	
+	public static final String BUILDING_RULES_CONTENT = "<p>This sub-menu contains information about individual sections of character building process. "
+															+ "Its topics should give you necessary pieces of information about background rules "
+															+ "that are used in <b><i>The Elder Scrolls: Skyrim</i> modded with <i>Perkus Maximus</i> perk overhaul "
+															+ "and <i>Disparity</i> character creation overhaul</b>.</p>"
+															+ "<h3>Perkus Maximus:</h3>"
+															+ "<p>This mod overhauls individual skill trees, perks and some combat mechanics. "
+															+ "This application is made with all <i>PerMa</i> modules in mind. You can find the mod "
+															+ "<a href=\"http://www.nexusmods.com/skyrim/mods/59849/?\">here</a>.</p>"
+															+ "<h3>Disparity:</h3>"
+															+ "<p>This mod overhauls races, their abilities and character creation in general. "
+															+ "This application currently counts with all <i>disparity</i> (race, gender...) options on "
+															+ "and with initial skill bonuses set to <i>modifiers</i> option. "
+															+ "You can find the mod <a href=\"http://www.nexusmods.com/skyrim/mods/63354/?\">here</a>.</p>";
+	
+	public static final String TERMS_CONTENT = "There's supposed to be something.";
+	
+	public static final String BUILD_BASE_CONTENT = "There's supposed to be something.";
+	
+	public static final String BUILD_LEVELING_CONTENT = "There's supposed to be something.";
+	
+	public static final String APPLICATION_CONTENT = "There's supposed to be something.";
+	
+	public static final String MAIN_MENU_CONTENT = "There's supposed to be something.";
+	
+	public static final String LEFT_PANE_CONTENT = "There's supposed to be something.";
+	
+	public static final String GENERAL_TAB_CONTENT = "There's supposed to be something.";
+	
+	public static final String SKILL_TAB_CONTENT = "There's supposed to be something.";
+	
+	public static final String ATTR_TAB_CONTENT = "There's supposed to be something.";
+
+}
--- src/view/HelpWindow.java
+++ src/view/HelpWindow.java
@@ -16,46 +16,20 @@ import view.recordObjects.HelpRec;
 
 public class HelpWindow extends Stage {
 	
-	private static final String ROOT_LABEL = "HELP MENU";
-	private static final String ROOT_CONTENT = "Choose the topic from the menu...";
-	
-	private static final String ABOUT_LABEL = "About the application";
-	private static final String ABOUT_CONTENT = "There's supposed to be something.";
-	
-	private static final String GUIDE_LABEL = "Builder guide";
-	private static final String GUIDE_CONTENT = "There's supposed to be something.";
-	
-	private static final String BUILDING_RULES_LABEL = "Character building rules";
-	private static final String BUILDING_RULES_CONTENT = "There's supposed to be something.";
-	
-	private static final String TERMS_LABEL = "Explanation terms";
-	private static final String TERMS_CONTENT = "There's supposed to be something.";
-	
-	private static final String BUILD_BASE_LABEL = "Build base";
-	private static final String BUILD_BASE_CONTENT = "There's supposed to be something.";
-	
-	private static final String BUILD_LEVELING_LABEL = "Build leveling";
-	private static final String BUILD_LEVELING_CONTENT = "There's supposed to be something.";
-	
-	private static final String APPLICATION_LABEL = "Application usage";
-	private static final String APPLICATION_CONTENT = "There's supposed to be something.";
-	
-	private static final String MAIN_MENU_LABEL = "Main Menu description";
-	private static final String MAIN_MENU_CONTENT = "There's supposed to be something.";
-	
-	private static final String LEFT_PANE_LABEL = "Left Pane description";
-	private static final String LEFT_PANE_CONTENT = "There's supposed to be something.";
-	
-	private static final String GENERAL_TAB_LABEL = "General Tab description";
-	private static final String GENERAL_TAB_CONTENT = "There's supposed to be something.";
-	
-	private static final String SKILL_TAB_LABEL = "Skills & Perks Tab description";
-	private static final String SKILL_TAB_CONTENT = "There's supposed to be something.";
-	
-	private static final String ATTR_TAB_LABEL = "Attributes Distribution Tab description";
-	private static final String ATTR_TAB_CONTENT = "There's supposed to be something.";
-	
 	private static final String TITLE = "Application guide";
+	private static final String ROOT_LABEL = "HELP MENU";
+	private static final String ABOUT_LABEL = "About the application";
+	private static final String GUIDE_LABEL = "Builder guide";
+	private static final String BUILDING_RULES_LABEL = "Character building rules";
+	private static final String TERMS_LABEL = "Terms explanation";
+	private static final String BUILD_BASE_LABEL = "Build base";
+	private static final String BUILD_LEVELING_LABEL = "Build leveling";
+	private static final String APPLICATION_LABEL = "Application usage";
+	private static final String MAIN_MENU_LABEL = "Main Menu description";
+	private static final String LEFT_PANE_LABEL = "Left Pane description";
+	private static final String GENERAL_TAB_LABEL = "General Tab description";
+	private static final String SKILL_TAB_LABEL = "Skills & Perks Tab description";
+	private static final String ATTR_TAB_LABEL = "Attributes Distribution Tab description";
 	
 	private static final int MIN_WIDTH = 800;
 	private static final int MIN_HEIGHT = 600;
@@ -108,19 +82,19 @@ public class HelpWindow extends Stage {
 		helpMenu.setStyle(Main.SECONDARY_LABEL_STYLE);
 		helpMenu.getSelectionModel().getSelectedItems().addListener((ListChangeListener.Change<? extends TreeItem<HelpRec>> change) -> setHelpContent());
 		
-		TreeItem<HelpRec> rootItem = new TreeItem<HelpRec>(new HelpRec(ROOT_LABEL, ROOT_CONTENT));
-		TreeItem<HelpRec> aboutItem = new TreeItem<HelpRec>(new HelpRec(ABOUT_LABEL, ABOUT_CONTENT));
-		TreeItem<HelpRec> guideItem = new TreeItem<HelpRec>(new HelpRec(GUIDE_LABEL, GUIDE_CONTENT));
-		TreeItem<HelpRec> buildingRulesItem = new TreeItem<HelpRec>(new HelpRec(BUILDING_RULES_LABEL, BUILDING_RULES_CONTENT));
-		TreeItem<HelpRec> termsItem = new TreeItem<HelpRec>(new HelpRec(TERMS_LABEL, TERMS_CONTENT));
-		TreeItem<HelpRec> buildBaseItem = new TreeItem<HelpRec>(new HelpRec(BUILD_BASE_LABEL, BUILD_BASE_CONTENT));
-		TreeItem<HelpRec> buildLevelingItem = new TreeItem<HelpRec>(new HelpRec(BUILD_LEVELING_LABEL, BUILD_LEVELING_CONTENT));
-		TreeItem<HelpRec> applicationItem = new TreeItem<HelpRec>(new HelpRec(APPLICATION_LABEL, APPLICATION_CONTENT));
-		TreeItem<HelpRec> mainMenuItem = new TreeItem<HelpRec>(new HelpRec(MAIN_MENU_LABEL, MAIN_MENU_CONTENT));
-		TreeItem<HelpRec> leftPaneItem = new TreeItem<HelpRec>(new HelpRec(LEFT_PANE_LABEL, LEFT_PANE_CONTENT));
-		TreeItem<HelpRec> generalTabItem = new TreeItem<HelpRec>(new HelpRec(GENERAL_TAB_LABEL, GENERAL_TAB_CONTENT));
-		TreeItem<HelpRec> skillTabItem = new TreeItem<HelpRec>(new HelpRec(SKILL_TAB_LABEL, SKILL_TAB_CONTENT));
-		TreeItem<HelpRec> attrTabItem = new TreeItem<HelpRec>(new HelpRec(ATTR_TAB_LABEL, ATTR_TAB_CONTENT));
+		TreeItem<HelpRec> rootItem = new TreeItem<HelpRec>(new HelpRec(ROOT_LABEL, HelpTexts.ROOT_CONTENT));
+		TreeItem<HelpRec> aboutItem = new TreeItem<HelpRec>(new HelpRec(ABOUT_LABEL, HelpTexts.ABOUT_CONTENT));
+		TreeItem<HelpRec> guideItem = new TreeItem<HelpRec>(new HelpRec(GUIDE_LABEL, HelpTexts.GUIDE_CONTENT));
+		TreeItem<HelpRec> buildingRulesItem = new TreeItem<HelpRec>(new HelpRec(BUILDING_RULES_LABEL, HelpTexts.BUILDING_RULES_CONTENT));
+		TreeItem<HelpRec> termsItem = new TreeItem<HelpRec>(new HelpRec(TERMS_LABEL, HelpTexts.TERMS_CONTENT));
+		TreeItem<HelpRec> buildBaseItem = new TreeItem<HelpRec>(new HelpRec(BUILD_BASE_LABEL, HelpTexts.BUILD_BASE_CONTENT));
+		TreeItem<HelpRec> buildLevelingItem = new TreeItem<HelpRec>(new HelpRec(BUILD_LEVELING_LABEL, HelpTexts.BUILD_LEVELING_CONTENT));
+		TreeItem<HelpRec> applicationItem = new TreeItem<HelpRec>(new HelpRec(APPLICATION_LABEL, HelpTexts.APPLICATION_CONTENT));
+		TreeItem<HelpRec> mainMenuItem = new TreeItem<HelpRec>(new HelpRec(MAIN_MENU_LABEL, HelpTexts.MAIN_MENU_CONTENT));
+		TreeItem<HelpRec> leftPaneItem = new TreeItem<HelpRec>(new HelpRec(LEFT_PANE_LABEL, HelpTexts.LEFT_PANE_CONTENT));
+		TreeItem<HelpRec> generalTabItem = new TreeItem<HelpRec>(new HelpRec(GENERAL_TAB_LABEL, HelpTexts.GENERAL_TAB_CONTENT));
+		TreeItem<HelpRec> skillTabItem = new TreeItem<HelpRec>(new HelpRec(SKILL_TAB_LABEL, HelpTexts.SKILL_TAB_CONTENT));
+		TreeItem<HelpRec> attrTabItem = new TreeItem<HelpRec>(new HelpRec(ATTR_TAB_LABEL, HelpTexts.ATTR_TAB_CONTENT));
 		
 		applicationItem.getChildren().addAll(mainMenuItem, leftPaneItem, generalTabItem, skillTabItem, attrTabItem);
 		buildingRulesItem.getChildren().addAll(termsItem, buildBaseItem, buildLevelingItem);
